dfbb00599bf015fa0a18ab0c9d353080b495b9eb
nero <zhanglei-js@secoo.com>
2019-06-05 22:49:42 +0800

    java agent 实践

--- agent/readme.md
+++ agent/readme.md
@@ -1,3 +1,25 @@
+### 个人理解
+    将agent程序放入到main函数的项目中，提供Instrumentation对象调用JVMTI开放出来的功能。
+    使得开发者可以对jvm进行一些灵活操作
+
+
+
+### ManiFest文件
+    Manifest-Version: 1.0
+    Archiver-Version: Plexus Archiver
+    Built-By: nero
+    Created-By: Apache Maven 3.6.0
+    Build-Jdk: 1.8.0_201
+    Main-Class: premain.Main
+    Premain-Class: premain.Premain
+
+
 ### 启动脚本：
-    java -javaagent:agent-1.0-SNAPSHOT.jar -jar agent-1.0-SNAPSHOT.jar 
+    java -javaagent:./agent-1.0-SNAPSHOT.jar -jar agent-1.0-SNAPSHOT.jar
+
+    -javaagent: 指明premain的代理程序，这个代理程序寻找的是agent，包含premain的jar
+    -jar : 指明main方法入口，这个指明的main项目的jar
+    
+    
+### Instrumentation对象
 
--- agent/src/main/java/premain/Premain.java
+++ agent/src/main/java/premain/Premain.java
@@ -1,13 +1,25 @@
 package premain;
 
-
-
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
 
 public class Premain {
 
 
-    public static void premain(String args){
-        System.out.println("this is the premain method...");
+
+    public static void premain(String args, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new Trans());
+    }
+
+
+    static class Trans implements ClassFileTransformer{
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+            System.out.println(className);
+            return null;
+        }
     }
 
 
